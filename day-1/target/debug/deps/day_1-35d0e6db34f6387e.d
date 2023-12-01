825464bca89744fe3571d22fa3a860c4f2b5be36
Rami Wohl <rami.wohl@nos.nl>
2023-12-01 18:20:37 +0100

    re-add gitignore

--- day-1/target/debug/deps/day_1-35d0e6db34f6387e.d
+++
@@ -1,5 +0,0 @@
-/Users/ramiwohl/Projects/advent-of-code-2023/day-1/target/debug/deps/day_1-35d0e6db34f6387e.rmeta: src/main.rs
-
-/Users/ramiwohl/Projects/advent-of-code-2023/day-1/target/debug/deps/day_1-35d0e6db34f6387e.d: src/main.rs
-
-src/main.rs:
--- day-1/target/debug/deps/day_1-de302c92803e1d13.d
+++
@@ -1,5 +0,0 @@
-/Users/ramiwohl/Projects/advent-of-code-2023/day-1/target/debug/deps/day_1-de302c92803e1d13: src/main.rs
-
-/Users/ramiwohl/Projects/advent-of-code-2023/day-1/target/debug/deps/day_1-de302c92803e1d13.d: src/main.rs
-
-src/main.rs:
