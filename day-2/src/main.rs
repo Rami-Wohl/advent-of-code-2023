c37da7f04b1a743898dc0e5bc0d379c51df7fd43
Rami Wohl <rami.wohl@nos.nl>
2023-12-02 14:16:38 +0100

    day-2 pt 2

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -3,19 +3,19 @@ use std::fs::read_to_string;
 
 fn main() {
     let lines: Vec<String> = read_lines("scores.txt");
-    let re_index: Regex = Regex::new(r"(\d{1,3})").unwrap();
+    //let re_index: Regex = Regex::new(r"(\d{1,3})").unwrap();
     let re_blue: Regex = Regex::new(r"(?<blue>\d{1,3})\ blue").unwrap();
     let re_green: Regex = Regex::new(r"(?<green>\d{1,3})\ green").unwrap();
     let re_red: Regex = Regex::new(r"(?<red>\d{1,3})\ red").unwrap();
-    const MAX_RED: i32 = 12;
-    const MAX_GREEN: i32 = 13;
-    const MAX_BLUE: i32 = 14;
+    // const MAX_RED: i32 = 12;
+    // const MAX_GREEN: i32 = 13;
+    // const MAX_BLUE: i32 = 14;
 
-    let mut ids: Vec<i32> = Vec::new();
+    let mut powers: Vec<i32> = Vec::new();
 
     for line in lines {
-        let mat = re_index.find(&line).unwrap();
-        let id: i32 = mat.as_str().parse().unwrap();
+        // let mat = re_index.find(&line).unwrap();
+        //let id: i32 = mat.as_str().parse().unwrap();
 
         let blues: Vec<i32> = re_blue
             .captures_iter(&line)
@@ -29,7 +29,7 @@ fn main() {
             })
             .collect();
 
-        let blues_exceeded = blues.iter().any(|&f| f > MAX_BLUE);
+        // let blues_exceeded = blues.iter().any(|&f| f > MAX_BLUE);
 
         let greens: Vec<i32> = re_green
             .captures_iter(&line)
@@ -43,7 +43,7 @@ fn main() {
             })
             .collect();
 
-        let greens_exceeded = greens.iter().any(|&f| f > MAX_GREEN);
+        // let greens_exceeded = greens.iter().any(|&f| f > MAX_GREEN);
 
         let reds: Vec<i32> = re_red
             .captures_iter(&line)
@@ -57,16 +57,19 @@ fn main() {
             })
             .collect();
 
-        let reds_exceeded = reds.iter().any(|&f| f > MAX_RED);
+        // let reds_exceeded = reds.iter().any(|&f| f > MAX_RED);
 
-        if blues_exceeded || greens_exceeded || reds_exceeded {
-            continue;
-        }
+        // if blues_exceeded || greens_exceeded || reds_exceeded {
+        //     continue;
+        // }
 
-        ids.push(id);
+        let power: i32 =
+            blues.iter().max().unwrap() * greens.iter().max().unwrap() * reds.iter().max().unwrap();
+
+        powers.push(power);
     }
 
-    let sum: i32 = ids.iter().sum();
+    let sum: i32 = powers.iter().sum();
 
     println!("sum is: {}", sum);
 }
