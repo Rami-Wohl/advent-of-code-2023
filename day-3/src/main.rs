88e905de0465f45afb39c69f1059bbe8ca72349d
Rami Wohl <rami.wohl@nos.nl>
2023-12-03 23:12:58 +0100

    day-3 pt 2

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,5 +1,6 @@
 use regex::Regex;
 use std::cmp;
+use std::collections::HashMap;
 use std::fs::read_to_string;
 
 struct Line {
@@ -19,12 +20,19 @@ struct Position {
     y: usize,
 }
 
+#[derive(Clone)]
+struct StarPosition {
+    num: i32,
+    pos: String,
+}
+
 fn main() {
     let lines: Vec<String> = read_lines("codes.txt");
     let re: Regex = Regex::new(r"(\d{1,3})").unwrap();
     let mut lines_vec: Vec<Line> = Vec::new();
     let mut codes: Vec<Code> = Vec::new();
     let mut no_adjacent_symbols: Vec<i32> = Vec::new();
+    let mut star_adjacent_nums: Vec<StarPosition> = Vec::new();
 
     for (line_num, line) in lines.iter().enumerate() {
         lines_vec.push(Line {
@@ -32,33 +40,29 @@ fn main() {
             line: line.to_string(),
         });
 
-        re.find_iter(line.as_str()).for_each(|mat| {
-            codes.push(Code {
-                line_num: line_num,
-                line_idx: mat.start(),
-                len: mat.len(),
-                num: mat.as_str().to_string(),
+        re.find_iter(line.as_str())
+            .for_each(|mat: regex::Match<'_>| {
+                codes.push(Code {
+                    line_num: line_num,
+                    line_idx: mat.start(),
+                    len: mat.len(),
+                    num: mat.as_str().to_string(),
+                });
             });
-        });
     }
 
-    codes.iter().for_each(|code| {
+    codes.iter().for_each(|code: &Code| {
         let mut positions: Vec<Position> = Vec::new();
-        let line_length = lines_vec[0].line.len();
-
-        // println!(
-        //     "num: {}, idx: {}, line: {}, len: {}",
-        //     code.num, code.line_idx, code.line_num, code.len
-        // );
+        let line_length: usize = lines_vec[0].line.len();
 
         //upper
         if code.line_num > 0 {
-            let low_boundary = if code.line_idx == 0 {
+            let low_boundary: usize = if code.line_idx == 0 {
                 0
             } else {
                 code.line_idx - 1
             };
-            let high_boundary = cmp::min(code.line_idx + code.len + 1, line_length);
+            let high_boundary: usize = cmp::min(code.line_idx + code.len + 1, line_length);
 
             (low_boundary..high_boundary).for_each(|x| {
                 positions.push(Position {
@@ -70,14 +74,14 @@ fn main() {
 
         //lower
         if code.line_num + 1 < lines_vec.len() {
-            let low_boundary = if code.line_idx == 0 {
+            let low_boundary: usize = if code.line_idx == 0 {
                 0
             } else {
                 code.line_idx - 1
             };
-            let high_boundary = cmp::min(code.line_idx + code.len + 1, line_length - 1);
+            let high_boundary: usize = cmp::min(code.line_idx + code.len + 1, line_length - 1);
 
-            (low_boundary..high_boundary).for_each(|x| {
+            (low_boundary..high_boundary).for_each(|x: usize| {
                 positions.push(Position {
                     x: (x),
                     y: (code.line_num + 1),
@@ -94,62 +98,72 @@ fn main() {
         }
 
         //right
-
         if code.line_idx + code.len < line_length - 1 {
             positions.push(Position {
                 x: (code.line_idx + code.len),
                 y: (code.line_num),
             })
         }
-        // if code.num == "827" {
-        //     println!(
-        //         "lineidx + {}, linelen {}",
-        //         code.line_idx + code.len,
-        //         line_length
-        //     );
-        //     lines_vec
-        //         .iter()
-        //         .for_each(|lne| println!("numnum {}", lne.line_num));
 
-        //     positions
-        //         .iter()
-        //         .for_each(|pos| println!("num: {} x: {} y: {}", code.num, pos.x, pos.y));
-        // }
-
-        if positions.iter().all(|pos| {
-            let line = lines_vec
+        if positions.iter().all(|pos: &Position| {
+            let line: &Line = lines_vec
                 .iter()
-                .find(|line| line.line_num == pos.y)
+                .find(|line: &&Line| line.line_num == pos.y)
                 .unwrap();
-
-            // println!(
-            //     "num: {}, symbol: {}",
-            //     code.num,
-            //     line.line.chars().nth(pos.x).unwrap()
-            //);
 
             line.line.chars().nth(pos.x).unwrap() == '.'
         }) {
             no_adjacent_symbols.push(code.num.parse::<i32>().unwrap())
         }
-    });
 
-    no_adjacent_symbols
-        .iter()
-        .for_each(|num| println!("num: {}", num));
+        for pos in positions {
+            let line: &Line = lines_vec
+                .iter()
+                .find(|line: &&Line| line.line_num == pos.y)
+                .unwrap();
+
+            if line.line.chars().nth(pos.x).unwrap() == '*' {
+                star_adjacent_nums.push(StarPosition {
+                    num: code.num.parse::<i32>().unwrap(),
+                    pos: format!("{}-{}", pos.x, pos.y),
+                })
+            }
+        }
+    });
 
     let sum: i32 = no_adjacent_symbols.iter().sum();
 
     let sum_all: i32 = codes
         .iter()
-        .map(|code| code.num.parse::<i32>().unwrap())
+        .map(|code: &Code| code.num.parse::<i32>().unwrap())
         .collect::<Vec<i32>>()
         .iter()
         .sum();
 
-    let diff = sum_all - sum;
+    let diff: i32 = sum_all - sum;
 
     println!("sum of all numbers is: {}, diff is {}", sum_all, diff);
+
+    let mut groups: HashMap<String, Vec<i32>> = HashMap::new();
+
+    star_adjacent_nums
+        .into_iter()
+        .for_each(|star_pos: StarPosition| {
+            let group: &mut Vec<i32> = groups.entry(star_pos.pos).or_insert(vec![]);
+            group.push(star_pos.num);
+        });
+
+    let mut gear_ratios: Vec<i32> = Vec::new();
+
+    for (id, group) in groups {
+        if group.len() == 2 {
+            gear_ratios.push(group[0] * group[1])
+        }
+    }
+
+    let sum_gear_ratios: i32 = gear_ratios.iter().sum();
+
+    println!("sum of all gear ratios is: {}", sum_gear_ratios);
 }
 
 fn read_lines(filename: &str) -> Vec<String> {
