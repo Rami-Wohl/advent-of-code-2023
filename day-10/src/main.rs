b5b49176062b0cfae9462b848df8a923fd982b35
Rami Wohl <rami.wohl@nos.nl>
2023-12-11 16:40:10 +0100

    day-10 pt 1 minor update

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::read_to_string;
 use std::time::SystemTime;
+use std::usize;
 
 #[derive(Clone, PartialEq)]
 enum Move {
@@ -32,6 +33,13 @@ fn get_next_steps(lines: &Vec<String>, current: &Point) -> Vec<Point> {
                 });
             }
 
+            if current.x < lines.first().unwrap().len() + 1 {
+                next_steps.push(Point {
+                    last_move: Move::RIGHT,
+                    x: current.x + 1,
+                    y: current.y,
+                })
+            };
             if current.y < lines.len() + 1 {
                 next_steps.push(Point {
                     last_move: Move::DOWN,
@@ -47,14 +55,6 @@ fn get_next_steps(lines: &Vec<String>, current: &Point) -> Vec<Point> {
                     y: current.y,
                 })
             }
-
-            if current.x < lines.first().unwrap().len() + 1 {
-                next_steps.push(Point {
-                    last_move: Move::RIGHT,
-                    x: current.x + 1,
-                    y: current.y,
-                })
-            };
         }
         (Move::UP, '|') => {
             if current.y > 0 {
@@ -182,7 +182,7 @@ fn is_path_valid(lines: &Vec<String>, current: &Point) -> bool {
     }
 }
 
-fn step(lines: &Vec<String>, current: Point, path: &mut Vec<Point>) -> bool {
+fn step(lines: &Vec<String>, current: &Point, path: &mut Vec<Point>) -> bool {
     let valid_path = is_path_valid(&lines, &current);
 
     let reached_end: bool =
@@ -197,7 +197,7 @@ fn step(lines: &Vec<String>, current: Point, path: &mut Vec<Point>) -> bool {
     }
 
     if reached_end {
-        path.push(current);
+        path.push(current.clone());
         return true;
     }
 
@@ -206,7 +206,7 @@ fn step(lines: &Vec<String>, current: Point, path: &mut Vec<Point>) -> bool {
     let next_steps: Vec<Point> = get_next_steps(&lines, &current);
 
     for next in next_steps {
-        if step(lines, next, path) {
+        if step(lines, &next, path) {
             return true;
         }
     }
@@ -219,7 +219,7 @@ fn step(lines: &Vec<String>, current: Point, path: &mut Vec<Point>) -> bool {
 fn main() {
     let start_time: SystemTime = SystemTime::now();
 
-    let lines: Vec<String> = read_lines("input.txt");
+    let lines: Vec<String> = read_lines("test-input-2.txt");
 
     let mut start: Point = Point {
         last_move: Move::NONE,
@@ -235,17 +235,60 @@ fn main() {
         }
     }
 
-    println!("start x: {}, start y: {}", start.x, start.y);
+    // println!("start x: {}, start y: {}", start.x, start.y);
 
     let path: &mut Vec<Point> = &mut Vec::new();
 
-    step(&lines, start, path);
+    step(&lines, &start, path);
 
-    // path.iter()
-    //     .for_each(|point: &Point| println!("point x: {}, point y: {}", point.x, point.y));
+    let points: Vec<String> = vec![];
 
     println!("path is {} steps long", path.len() - 1);
     println!("max distance is {}", ((path.len() - 1) / 2));
+
+    struct PathPoint {
+        x: usize,
+        y: usize,
+    }
+
+    let points_split_vec: Vec<PathPoint> = points
+        .iter()
+        .map(|p| {
+            let x = p.split_once("-").unwrap().0.parse::<usize>().unwrap();
+            let y = p.split_once("-").unwrap().1.parse::<usize>().unwrap();
+
+            PathPoint { x: x, y: y }
+        })
+        .collect();
+
+    for (i, line) in lines.iter().enumerate() {
+        let line_path = line
+            .chars()
+            .into_iter()
+            .enumerate()
+            .map(|(j, _c)| {
+                if points_split_vec.iter().any(|p| p.x == j && p.y == i) {
+                    'O'
+                } else if start.x == j && start.y == i {
+                    'S'
+                } else {
+                    match path.iter().any(|p| p.x == j && p.y == i) {
+                        true => '*',
+                        false => '.',
+                    }
+                }
+            })
+            .collect::<String>();
+        println!("{}", line_path)
+    }
+
+    println!(
+        "out of {} points, {} belong to path, {} are inside of loop and {} are outside of loop",
+        lines.first().unwrap().len() * lines.len(),
+        path.len() - 1,
+        points.len(),
+        lines.first().unwrap().len() * lines.len() - (path.len() - 1 + points.len())
+    );
 
     println!("done in {:?}", start_time.elapsed().unwrap())
 }
